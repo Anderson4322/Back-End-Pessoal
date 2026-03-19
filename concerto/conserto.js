995f22378007e016151827e001f0fa2a6f6c74da
Dudis140812 <maria.e.silva938@aluno.senai.br>
2026-03-19 13:26:19 -0300

    conserto atualizado

---
+++ concerto/conserto.js
@@ -0,0 +1,96 @@
+const openButtons = document.querySelector('#abrirMod'); 
+const modal = document.querySelector("#modal-1"); 
+openButtons.addEventListener("click", () => {  
+modal.showModal(); //show ou showModal 
+}); 
+
+const closeButtons = document.querySelector('#sair'); 
+closeButtons.addEventListener('click', () =>{ 
+modal.close(); //fechar modal 
+});
+
+const btn = document.querySelector("#confirmar");
+const input = document.querySelectorAll("input");
+const select = document.querySelector("select");
+const details = document.querySelector("details");
+
+document.querySelector('form').addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const nome = document.querySelector("#nome").value;
+  const descricao = document.querySelector("#descricao").value;
+  const tipoeletronico = document.querySelector("#tipoeletronico").value;
+  const modelo = document.querySelector("#modelo").value;
+  const telefone = document.querySelector("#telefone").value;
+  // const comentario = document.querySelector("#comentario").value;
+  console.log(nome, descricao, tipoeletronico, modelo);
+ 
+  const resposta = await fetch("http://localhost:3000/cad_pedidos", {
+    method: "POST",
+    headers: { "Content-Type": "application/JSON" },
+    body: JSON.stringify({
+        nome,
+        descricao,
+        tipoeletronico,
+        modelo,
+        telefone
+    }),
+  });
+  if(resposta.status == 200){
+    return window.location.reload();
+  }else{
+    alert("Erro ao cadastro de pedido")
+  }
+});
+
+
+
+const conteudo = document.querySelector(".grade-posts");
+
+window.addEventListener("load", async () => {
+  const resposta = await fetch("http://localhost:3000/pedidos");
+  const produto = await resposta.json();
+
+  produto.map((p) => {
+console.log(p);
+    const card = document.createElement("div");
+    card.classList.add("caixa-problema");
+
+    const tipo = document.createElement("p");
+    tipo.className = "p2";
+    tipo.textContent = p.tipoeletronico;
+
+    const nome = document.createElement("h3");
+    nome.className = "p1";
+    nome.textContent = p.nome;
+
+    const telefone = document.createElement("h5");
+    telefone.textContent = `Número de Contato: ${p.telefone}`;
+
+    const linha = document.createElement("hr");
+    linha.style.border = "none";
+    linha.style.borderTop = "1px solid #eee";
+
+    const modelo = document.createElement("h4");
+    modelo.className = "mod";
+    modelo.textContent = `Modelo: ${p.modelo}`;
+
+    const descricao = document.createElement("p");
+    descricao.textContent = `Descrição: ${p.descricao}`;
+
+    const valor = document.createElement("h3");
+    valor.className = "valor1";
+    valor.textContent = `Valor: R$${p.valor}`;
+
+    card.appendChild(tipo);
+    card.appendChild(nome);
+    card.appendChild(telefone);
+    card.appendChild(linha);
+    card.appendChild(modelo);
+    card.appendChild(descricao);
+    card.appendChild(valor);
+
+    conteudo.appendChild(card);
+  });
+});
+  
+  
